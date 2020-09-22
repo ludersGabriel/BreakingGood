1e3d564cdccf9262015c9639d5abb03a0f2d2653
Luders <gabeluders@gmail.com>
2020-09-22 18:20:40 -0300

    brakingood.py finalziado?

--- Files/breakingood.py
+++ Files/breakingood.py
@@ -5,12 +5,33 @@ import time
 import json
 import pefile
 import magic
-from libraries import data_treatment, injection, malware_manipulation, vtapi
+from libraries import data_treatment, malware_manipulation, vtapi
 
 
 class Breakingood():
     @staticmethod
-    def __check_exe(path):
+    def open_file(name, mode):
+        file = open(name, mode)
+        if file.closed:
+            print("Could not open {name}".format(name=name))
+            sys.exit(1)
+        return file
+
+
+    @staticmethod
+    def arg_parser():
+        parser = argparse.ArgumentParser(description="Modifies original malware by \
+                                    appending bytes of data and strings to it")
+        parser.add_argument("folder", type=str, help="name of the temp_folder to save data")
+        parser.add_argument("malware", type=str, help="name of the malware")
+        parser.add_argument("goodware", type=str, help="name of the goodware")
+        parser.add_argument("--nsave", action="store_true", help="Doesnt save files when used")
+        parser.add_argument("--ncolor", action="store_true", help="Doesnt display colors in output")
+        parser.add_argument("--nprint", action="store_true", help="DOesnt print the output")
+        return parser.parse_args()
+
+    @staticmethod
+    def check_exe(path):
         s = magic.from_file(path)
         s = s.split(" ")
 
@@ -27,11 +48,12 @@ class Breakingood():
         return s[0]
 
     @staticmethod
-    def __files_in(path):
+    def files_in(path):
         files = os.popen("ls {folder}".format(folder=path)).read()
         files = list(files.split("\n"))
         del files[len(files) - 1]
 
+        if not files: return None
         return files
 
     @staticmethod
@@ -59,10 +81,76 @@ class Breakingood():
         dis = malware_manipulation.disassemble(resultsPath)
         dis.replace_int3_addSub(malwarePath)
 
+    @staticmethod
+    def handle_virus_total(resultsPath, nsave=False):
+        files = Breakingood.files_in(resultsPath)
+        if not files:
+            print("No files to analyze")
+            sys.exit(2)
+        api = vtapi.handle_api(resultsPath, nsave)
+
+        for file in files:
+            api_file = "{folder}/{file}".format(folder=resultsPath, file=file)
+            scan = api.file_scan(api_file)
+            
+            report = api.file_report(scan["md5"])
+            print("[+]Waiting for Virus Total Response..")
+            while report["response_code"] != 1:
+                time.sleep(60)
+                print("[+]Waiting for Virus Total Response..")
+                report = api.file_report(scan["md5"])
+
+            if not nsave:
+                name = "{folder}/{file}.vt".format(folder=resultsPath, file=file)
+                save_file = Breakingood.open_file(name, "w")
+                save_file.write(json.dumps(report))
+                save_file.close()
+            time.sleep(60)
+
+    @staticmethod
+    def handle_results_table(resultsPath, ncolor, nprint):
+        data_manager = data_treatment.data_manager(resultsPath, ncolor, nprint)
+        data_manager.detection_table()
+
+
+    @staticmethod
+    def example(malwarePath=None, goodwarePath=None, resultsPath="./bgResults",
+                nsave=False, ncolor=False, nprint=False, raw=True, strings=True,
+                gwa=True, swap=True, replace=True):
+        if not malwarePath:
+            print("No malware specified")
+            sys.exit(2)
+        if not goodwarePath:
+            print("No goodware specified")
+            sys.exit(2)
+        
+        if raw:
+            print("[+]Working on raw byte append (.BYTE)..")
+            Breakingood.add_bytes(malwarePath, resultsPath)
+        if strings:
+            print("[+]Working on goodware string append (.STRING)..")
+            Breakingood.add_strings(malwarePath, goodwarePath, resultsPath)
+        if gwa:
+            print("[+]Working on goodware sections append (.GWA)..")
+            Breakingood.append_goodware_sections(malwarePath, goodwarePath, resultsPath)
+        if swap:
+            print("[+]Working on swap ret with nop (.SWAP)..")
+            Breakingood.swap_ret_nop(malwarePath, 3, resultsPath)
+        if replace:
+            print("[+]Working on replace int3 with addSub (.ADDSUB)..")
+            Breakingood.replace_int3_addSub(malwarePath, resultsPath)
+
+        Breakingood.handle_virus_total(resultsPath, nsave)
+        Breakingood.handle_results_table(resultsPath, ncolor, nprint)
 
 
 def main():
-    pass
+
+    args = Breakingood.arg_parser()
+
+    Breakingood.example(args.malware, args.goodware, args.folder, args.nsave,
+                        args.ncolor, args.nprint)
+
 
 if __name__ == "__main__":
     main()
--- Files/libraries/vtapi.py
+++ Files/libraries/vtapi.py
@@ -1,16 +1,21 @@
 import requests
+import sys
 
 class handle_api:
     report_url = "https://www.virustotal.com/vtapi/v2/file/report"
     scan_url = "https://www.virustotal.com/vtapi/v2/file/scan"
     #VIRUS TOTAL PERSONAL KEY AS STRING
-    key = ""
+    key = "7c4c63de83977f7e28e966435e83d672302ac8d9c07e31741973d572fbaaee00"
 
     def __init__(self, temp_dir, nsave):
         self.temp_dir = temp_dir
         self.nsave = nsave
     
     def file_scan(self, malware):
+        if not self.key:
+            print("No key add in libraries/vtapi.py")
+            sys.exit(2)
+
         params = {"key": self.key}
         files = {"file": (malware, open(malware, "rb"))}
         response = requests.post(self.scan_url, files=files, params=params)
